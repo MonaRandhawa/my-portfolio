6a5b5c70eceb77b9271b5c3d7e26bf535679a93a
GitHub User Name <email>
2021-04-05 18:46:05 -0600

    MR - deploy

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as HashRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Experience from "./pages/Experience";
@@ -13,14 +13,14 @@ function App() {
     <>
     <Navbar />
 
-    <HashRouter>
+    <Router>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/about" component={About} />
         <Route exact path="/experience" component={Experience} />
         <Route exact path="/projects" component={AllProjects} />
       </Switch>
-    </HashRouter>
+    </Router>
 
     </>
   );
